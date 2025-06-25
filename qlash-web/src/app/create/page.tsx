57e28e56a0f8bbbbd113f9328ee47af1e3274808
fmael <mael.fath@ynov.com>
2025-06-25 13:41:40 +0200

    Create page and join edit

--- qlash-web/src/app/create/page.tsx
+++ qlash-web/src/app/create/page.tsx
@@ -6,12 +6,12 @@ const Join = () => {
   return (
     <div className='bg-white h-screen text-black flex flex-row items-center justify-evenly'>
         <div className='flex flex-col items-center justify-center h-full'>
-            <h2 className='text-2xl font-bold mb-6 text-center'>Join</h2>
+            <h2 className='text-2xl font-bold mb-6 text-center'>Create</h2>
             <form className='w-96'>
                 <div className='mb-4'>
-                <Input type='text' id='code' className='w-full p-2 border-b border-black placeholder:text-black' required placeholder='Code Pine ta mère'/>
+                <Input type='text' id='username' className='w-full p-2 border-b border-black placeholder:text-black' required placeholder='Username'/>
                 </div>
-                <Button type='submit'>Join Game</Button>
+                <Button type='submit'>Create Game</Button>
             </form>
         </div>
     </div>
