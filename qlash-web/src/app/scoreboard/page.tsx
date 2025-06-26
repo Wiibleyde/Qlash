b0c2e888b2defdaa3d72276b9ff595cdfda23465
fmael <mael.fath@ynov.com>
2025-06-26 13:42:07 +0200

    Add scoreboard front page, start game page

---
+++ qlash-web/src/app/scoreboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import FinalScoreboard from "@/components/FinalScoreboard";
+import React from "react";
+
+const Scoreboard = () => {
+
+  return (
+    <FinalScoreboard />
+  );
+};
+
+export default Scoreboard;
