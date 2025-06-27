7fae8a91b6c5651e30ed3758c965f7ed9ce0a3ee
Wiibleyde <nathan@bonnell.fr>
2025-06-27 15:05:02 +0200

    feat: add user route to main routes and create game route file
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- qlash-back/routes/index.ts
+++ qlash-back/routes/index.ts
@@ -3,12 +3,14 @@ import type { IRoute } from '../../qlash-shared/types/socket';
 import authRoute from './auth';
 import healthRoute from './health';
 import quizRoute from './quiz';
+import userRoute from './user';
 
 
 const routes: IRoute[] = [
     healthRoute,
     authRoute,
-    quizRoute
+    quizRoute,
+    userRoute
 ];
 
 export const registerRoutes = (app: Express) => {
