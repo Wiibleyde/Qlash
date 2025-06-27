cd09722889c4d329d426a6bb36a926ede7115136
Wiibleyde <nathan@bonnell.fr>
2025-06-27 10:42:15 +0200

    fix: add return type to createGameCode function and improve code readability
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- qlash-back/utils/game.ts
+++ qlash-back/utils/game.ts
@@ -1,10 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
+import { games } from '../events/webserver';
 
-export const createGameCode = () => {
+export const createGameCode = (): string => {
     const characters = '0123456789';
     let code = '';
     for (let i = 0; i < 6; i++) {
         code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    if (games.some(game => game.code === code)) {
+        return createGameCode();
     }
     return code;
 };
